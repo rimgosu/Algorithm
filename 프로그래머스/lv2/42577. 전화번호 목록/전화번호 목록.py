b8208a4561903fd5b85eb34a02aa58e7a4fed2ab
rimgosu <120752098+rimgosu@users.noreply.github.com>
2023-07-09 15:49:07 +0900

    [level 2] Title: 전화번호 목록, Time: 497.86 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,114 +1,12 @@
 def solution(phone_book):
     answer = True
-    
-    for i in range(len(phone_book)):
-        # print(i)
-        pre = phone_book[i]
-        try:
-            _phone_book = phone_book[i+1:] + phone_book[:i]
-        except:
-            _phone_book = phone_book[:i]
-        
-        for pnum in _phone_book:
-            if pre == pnum[0:len(pre)]:
-                return False
-    return answer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def solution(phone_book):
-    answer = True
-    if len(phone_book) > 100000:
-        return True
-        
-    
-    for i in range(len(phone_book)):
-        # print(i)
-        
-        pre = phone_book[i]
-        try:
-            _phone_book = phone_book[i+1:] + phone_book[:i]
-        except:
-            _phone_book = phone_book[:i]
-        
-        for pnum in _phone_book:
-            if pre == pnum[0:len(pre)]:
-                return False
-    return answer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def solution(phone_book):
-    answer = True
-    minNum = 1000000
-    bookSet = set()
-    bookList = []
-    for pNum in phone_book:
-        if len(pNum) < minNum:
-            minNum = len(pNum)
-    print(minNum)
-    
-    for pNum in phone_book:
-        bookSet.add(pNum[0:minNum])
-        bookList.append(pNum[0:minNum])
-    
-    for b in bookSet:
-        if bookList.count(b) >= 2:
-            return False
-    
-    return answer
-
-
-import random as rd
-
-def solution(phone_book):
-    
-    answer = True
-    if len(phone_book) > 100000:
-        if rd.randrange(0,2) == 0: 
-            return True 
-        else:
-            return False 
-        
-    
-    for i in range(len(phone_book)):
-        # print(i)
-        
-        pre = phone_book[i]
-        try:
-            _phone_book = phone_book[i+1:] + phone_book[:i]
-        except:
-            _phone_book = phone_book[:i]
-        
-        for pnum in _phone_book:
-            if pre == pnum[0:len(pre)]:
-                return False
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
     return answer
