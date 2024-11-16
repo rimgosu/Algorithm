d205b064290822ac4af2d5d5ee5b4f768d2dc599
jihyup-byun <120752098+rimgosu@users.noreply.github.com>
2024-11-16 13:08:11 +0900

    [level 1] Title: 모의고사, Time: 5.22 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,16 +1,11 @@
 def solution(answers):
-    patterns = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ]
+    lsts = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
+    corrects = [0, 0, 0]
+    for index, answer in enumerate(answers):
+        for lst_index, lst in enumerate(lsts):
+            if lst[index % len(lst)] == answer:
+                corrects[lst_index] +=1
     
-    scores = [0, 0, 0]
-    
-    for i, answer in enumerate(answers):
-        for j, pattern in enumerate(patterns):
-            if answer == pattern[i % len(pattern)]:
-                scores[j] += 1
-    
-    max_score = max(scores)
-    return [i + 1 for i, score in enumerate(scores) if score == max_score]
+    ans_lst = [index +1 for index, correct in enumerate(corrects) if correct == max(corrects)]
+    ans_lst.sort()
+    return ans_lst
